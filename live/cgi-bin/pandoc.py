e28f6f9648af4af14ac8a6248986af7ccfa13cf3
nzbr <mail@nzbr.de>
2023-07-11 02:26:45 +0200

    pandocode-live: markdown mode

--- live/cgi-bin/pandoc.py
+++ live/cgi-bin/pandoc.py
@@ -1,33 +1,57 @@
 # print every line back
+import os
 import subprocess
 import sys
 from io import BytesIO
+
 import numpy
 from PIL import Image
 from pdf2image import convert_from_bytes
+
 from pandocode.codeprocessor import process_code
 
-print("Content-type: image/webp")
+options = os.environ['QUERY_STRING'].split('&')
+
+output = 'pdf'
+extraOpt = []
+inputData = sys.stdin.read()
+content_type = 'image/webp'
+
+if 'markdown' in options:
+    extraOpt = ['--filter', 'pandocode', '--standalone', '--metadata=documentclass:standalone', '--metadata=classoption:preview']
+else:
+    extraOpt = ['--template', '/etc/template.tex']
+    inputData = process_code(inputData)
+
+if 'code' in options:
+    output = 'latex'
+    content_type = 'text/plain'
+
+print(f"Content-type: {content_type}")
 print("Access-Control-Allow-Origin: *")
 print("")
 sys.stdout.flush()
 
-processed = process_code(sys.stdin.read())
 process = subprocess.run(
     [
         'pandoc',
         '-f', 'markdown',
-        '-t', 'pdf',
-        '--template', '/etc/template.tex',
+        '-t', output,
+    ] + extraOpt + [
         '--pdf-engine', 'pdflatex',
         '--pdf-engine-opt', '-disable-write18',
         '-o', '-'
     ],
-    input=processed.encode('utf-8'),
+    input=inputData.encode('utf-8'),
     stdout=subprocess.PIPE,
 )
 if process.returncode != 0:
     exit(process.returncode)
+
+if 'code' in options:
+    sys.stdout.buffer.write(process.stdout)
+    sys.stdout.flush()
+    exit(0)
 
 images = convert_from_bytes(process.stdout)
 image = images[0]
@@ -35,9 +59,9 @@ image = images[0]
 # Remove background (https://stackoverflow.com/a/21228321/12852285)
 image = image.convert('RGBA')
 data = numpy.array(image)
-rgb = data[:,:,:3]
-color = [255, 255, 255] # Original value
-mask = numpy.all(rgb == color, axis = -1)
+rgb = data[:, :, :3]
+color = [255, 255, 255]  # Original value
+mask = numpy.all(rgb == color, axis=-1)
 data[mask] = [0, 0, 0, 0]
 image = Image.fromarray(data)
 
